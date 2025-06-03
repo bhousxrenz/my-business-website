63962e8c072af7a5716b4e6e70396702611b50c5
Unknownymous <118184104+bhousxrenz@users.noreply.github.com>
2025-06-03 20:39:17 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'; // <--- This line is essential for global styles
+import '../styles/globals.css'; 
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
