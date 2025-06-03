67262b60baf24acfa0c2ce1af7ce81b42a1945c9
Unknownymous <118184104+bhousxrenz@users.noreply.github.com>
2025-06-03 20:29:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add any other Next.js specific configurations here
 };
 
 module.exports = nextConfig;
