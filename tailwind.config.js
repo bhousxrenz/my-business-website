be9fead58efe8a27e4cb73e1a711175c091ac0ac
Unknownymous <118184104+bhousxrenz@users.noreply.github.com>
2025-06-03 20:30:27 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    // Add any other paths where you use Tailwind classes, e.g., for `src` directory:
-    // "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
